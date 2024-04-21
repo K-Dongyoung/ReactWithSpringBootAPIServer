bd30ef49efe2c773d4f68a0db5f3677725dbfed7
K.Dongyoung <doyokim75@gmail.com>
2024-04-21 13:53:32 +0900

    240421 CustomFileUtil.java

--- apiserver/src/main/java/org/zerok/apiserver/util/CustomFileUtil.java
+++ apiserver/src/main/java/org/zerok/apiserver/util/CustomFileUtil.java
@@ -1,12 +1,20 @@
 package org.zerok.apiserver.util;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 
 @Component
@@ -14,11 +22,48 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CustomFileUtil {
 
-    @Value("{org.zerock.upload.path}")
+    @Value("${org.zerock.upload.path}")
     private String uploadPath;
+
+    @PostConstruct
+    public void init() {
+
+        File tempFolder = new File(uploadPath);
+
+        if (!tempFolder.exists()) {
+            tempFolder.mkdirs();
+        }
+
+        uploadPath = tempFolder.getAbsolutePath();
+
+        log.info("----------------------------------------");
+        log.info("uploadPath: " + uploadPath);
+
+    }
 
     public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException {
 
-        return null;
+        if (files == null || files.isEmpty()) {
+            return null;
+        }
+
+        List<String> uploadNames = new ArrayList<>();
+
+        for(MultipartFile file : files) {
+
+            String savedName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+            Path savePath = Paths.get(uploadPath, savedName);
+
+            try {
+                Files.copy(file.getInputStream(), savePath);
+                uploadNames.add(savedName);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        } // end for
+
+        return uploadNames;
     }
 }
